9ff10fb8f06bf2514c12803e67986435fe79933c
shiv4006 <154675100+shiv4006@users.noreply.github.com>
2024-10-13 23:02:40 +0530

    Add files via upload

---
+++ Frontend Projects/2. Travelia: Explore the world/assets/script/script.js
@@ -0,0 +1,45 @@
+/**
+* Working with preloader here, when website loaded hide preloaded
+*/
+const preloader = document.querySelector("[data-preloader]");
+window.addEventListener("load", () => {
+    preloader.classList.add("remove");
+});
+
+/**
+ * add event on multiple elements 
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for(let i=0, len=elements.length; i<len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+/**
+ * navbar toggler for mobile
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function() {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+
+
+/**
+ *  Header
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function() {
+    header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
+});
